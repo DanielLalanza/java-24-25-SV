97f3433f3f695b9a5a50e78a485230a44bb440e3
DanielLalanza <daniel.lalanza01@gmail.com>
2024-10-04 13:43:32 +0200

    Random Homer

--- Simpson/src/Simpson.java
+++ Simpson/src/Simpson.java
@@ -1,22 +1,48 @@
+import java.util.Random;
 public class Simpson {
     //VISIBLE A NIVEL DE LA CALSE SIMPSON
     static char[][] tablero = new char[10][10];     //Array de arrays       //static pq lo de abajo es estatico
     //FIN
-
-    // Software de gestión de venta empresarial
     public static void rellenarTablero(char pers){
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 tablero[i][j] = pers;
             }
         }
-    }// rellenarTablero ('L');
+    }
+
+    public static void imprimirTablero()  {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("["+tablero[i][j]+"]");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void randomizarpj(char pers){
+        Random r = new Random();
+        for (int k = 0; k <10 ; k++) {
+            int rx = r.nextInt(10);
+            int ry = r.nextInt(10);
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    tablero[rx][ry] = pers ;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        //System.out.printf("Hello and welcome!");
                                 // int contador = 0;
         char personaje = '#';   //Definir como caracter (char) comillas 'simples'
         rellenarTablero(personaje);
-                                // contador = 10
-        rellenarTablero(contador);
+        //imprimirTablero();
+
+        personaje = 'H';
+        randomizarpj(personaje);
+       // rellenarTablero(personaje);
+        imprimirTablero();
     }
 }
