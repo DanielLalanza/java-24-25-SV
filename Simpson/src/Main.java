cc917bc1386e6d7dfbd88b3088ecaa2d36d26457
DanielLalanza <daniel.lalanza01@gmail.com>
2024-10-04 11:44:00 +0200

    Simpson java

---
+++ Simpson/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    // Software de gestión de venta empresarial
+    public static void rellenarTablero(){
+        
+    }
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+    }
+}
