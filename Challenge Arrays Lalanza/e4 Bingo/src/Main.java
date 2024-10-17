5a1829ee8e26c5e7e7a8ecb4d5949d2696ced678
DanielLalanza <daniel.lalanza01@gmail.com>
2024-10-17 13:53:52 +0200

    Update Main.java

--- Challenge Arrays Lalanza/e4 Bingo/src/Main.java
+++ Challenge Arrays Lalanza/e4 Bingo/src/Main.java
@@ -6,7 +6,12 @@ public class Main {
         Random r = new Random();
         for (int i = 0; i <3 ; i++) {
             for (int j = 0; j <9 ; j++) {
-                matriz[i][j] = r.nextInt(10 + (j*10), 19 + (j*10));
+                int numRandom;
+                do {
+                    numRandom = r.nextInt(10 + (j*10), 19 + (j*10));
+                } while (!(matriz[i][j] == matriz[i][j] || matriz[i][j] == matriz[i][j]));
+                matriz[i][j] = numRandom;
+
             }
         }
     }
