c68076ec59450a335d39bbf76c814a8201114f36
DanielLalanza <daniel.lalanza01@gmail.com>
2024-10-17 13:38:35 +0200

    Bingo

---
+++ Challenge Arrays Lalanza/e4 Bingo/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Main {
+    public static int[][] matriz = new int[3][9];
+    public static void rellenarMatriz(){
+        Random r = new Random();
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j <9 ; j++) {
+                matriz[i][j] = r.nextInt(10 + (j*10), 19 + (j*10));
+            }
+        }
+    }
+    public static void imprimirMariz(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <9; j++) {
+                System.out.print(matriz[i][j]+"  ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        rellenarMatriz();
+        imprimirMariz();
+    }
+}
