3569b4dcbc938d0e482dd6cee441937147bfaa0f
DanielLalanza <daniel.lalanza01@gmail.com>
2024-10-19 16:21:34 +0200

    Ordenar matriz

--- Challenge Arrays Lalanza/e4 Bingo/src/Main.java
+++ Challenge Arrays Lalanza/e4 Bingo/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class Main {
     public static int[][] matriz = new int[3][9];
@@ -15,6 +16,21 @@ public class Main {
             }
         }
     }
+
+    public static void ordenarColumnas() {
+        for (int j = 0; j < 9; j++) {
+            int[] columna = new int[3];
+            for (int i = 0; i < 3; i++) {
+                columna[i] = matriz[i][j];
+            }
+            // Ordenamos la columna usando Arrays.sort
+            Arrays.sort(columna);
+            for (int i = 0; i < 3; i++) {
+                matriz[i][j] = columna[i];
+            }
+        }
+    }
+
     public static void imprimirMariz(){
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j <9; j++) {
@@ -24,8 +40,12 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
+        System.out.println("SIN ORDENAR");
         rellenarMatriz();
         imprimirMariz();
+        System.out.println("ORDENADO");
+        ordenarColumnas();
+        imprimirMariz();
+
     }
 }
