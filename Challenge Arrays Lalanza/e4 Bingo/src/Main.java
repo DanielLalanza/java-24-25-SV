3ce6b647dee970b5e47ec4000858edaed8ce8df0
DanielLalanza <daniel.lalanza01@gmail.com>
2024-10-21 13:52:06 +0200

    21/10/24 [13:52]

--- Challenge Arrays Lalanza/e4 Bingo/src/Main.java
+++ Challenge Arrays Lalanza/e4 Bingo/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static int[][] matriz = new int[3][9];
+    public static String[][] matrizString = new String[3][9];
     // Rellenar la matriz siguiendo los parametros pedidos en el ejercicio
     public static void rellenarMatriz(){
         Random r = new Random();
@@ -42,6 +43,39 @@ public class Main {
             System.out.println();
         }
     }
+
+    public static void imprimirCadena(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <9; j++) {
+                System.out.print(matrizString[i][j]+"  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void tachar() {
+        Random r = new Random();
+        int rx;
+        int ry;
+        for (int i = 0; i < 12; i++) {
+            do {
+                rx = r.nextInt(0,3);
+                ry = r.nextInt(0,9);
+            } while (matriz[rx][ry] == -1);
+            matriz[rx][ry]=-1;
+        }
+    }
+    public static void cadenaTachado(){
+        for (int i = 0; i <3; i++) {
+            for (int j = 0; j <9; j++) {
+                if (matriz[i][j] == -1){
+                    matrizString[i][j]="XX";
+                } else {
+                matrizString[i][j]= String.valueOf(matriz[i][j]);}
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("SIN ORDENAR");
         rellenarMatriz();
@@ -49,6 +83,10 @@ public class Main {
         System.out.println("ORDENADO");
         ordenarColumnas();
         imprimirMariz();
+        System.out.println("TACHADO");
+        tachar();
+        cadenaTachado();
+        imprimirCadena();
 
     }
 }
