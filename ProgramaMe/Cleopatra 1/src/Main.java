41d20ea8c6cda3cf88bd3c0152f6eb2e2cc872fa
DanielLalanza <daniel.lalanza01@gmail.com>
2024-10-24 16:16:35 +0200

    Update Main.java

--- ProgramaMe/Cleopatra 1/src/Main.java
+++ ProgramaMe/Cleopatra 1/src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numRepeticiones = sc.nextInt();
+
         for (int i = 0; i < numRepeticiones; i++) {
             int numA = sc.nextInt();
             int numB = sc.nextInt();
             int numC = sc.nextInt();
+
 
             if (numA * numB > 0) {
                 distA = Math.abs(numA - numB);
@@ -16,14 +18,14 @@ public class Main {
             }
 
             if (numC * numB > 0) {
-                distC = Math.abs(numA - numB);
+                distC = Math.abs(numC - numB);
             } else {
-                distC = Math.abs(numA - numB) - 1;
+                distC = Math.abs(numC - numB) - 1;
             }
 
             if (distA>distC){
                 System.out.println(numC);
-            } else if (distC>distA){
+            } else if (distA<distC){
                 System.out.println(numA);
             } else {
                 System.out.println("EMPATE");
