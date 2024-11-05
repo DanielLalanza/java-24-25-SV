dc32bdc8d01f72e4a427a0c8fccb3bd5ea6f1fea
Daniel Lalanza <daniel.lalanza01@gmail.com>
2024-11-05 12:37:17 +0100

    barras

---
+++ ProgramaMe/test/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ejecucion del programa
+        switch( numPregunta ){
+            case 0:
+                pregunta = "En que año nacio Albert Einstein ";
+                respuesta = 1893 ;
+                break ;
+            case 1:
+                pregunta = "En que año nacio Federico Garica Lorca ";
+                respuesta = 1893 ;
+                break ;
+            case 2:
+                pregunta = "En que año nacio Miguel Indurain ";
+                respuesta = 1893 ;
+                break ;
+        }
+
+    }
+}
